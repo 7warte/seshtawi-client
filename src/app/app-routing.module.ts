404410746560c8341c5039b6260ed22c3335cd15
7warte <zwarte336@gmail.com>
2023-11-21 10:43:32 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocumentationComponent } from './components/documentation/documentation.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'docs', component: DocumentationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/documentation/documentation.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+interface BlogArticle  {
+
+  timestamp: Date,
+  main_title:string,
+  subtitle_1:string,
+  body_1:string,
+  subtitle_2:string,
+  body_2:string,
+  caption:string,
+  tags:string[]
+
+
+
+}
+
+
+@Component({
+  selector: 'app-documentation',
+  templateUrl: './documentation.component.html',
+  styleUrls: ['./documentation.component.scss']
+})
+
+
+
+export class DocumentationComponent {
+
+
+
+
+
+
+   new_article:BlogArticle = {
+
+    timestamp:new Date('2023-11-21T15:45:00Z'),
+    main_title:'Main Title',
+    subtitle_1:'subtitle 1',
+    body_1: 'body 1',
+    subtitle_2:'subtitle_2',
+    body_2:'body 2',
+    caption:'caption',
+    tags:['fornite', 'new year', 'christmas']
+
+  }
+
+  
+
+
+}
