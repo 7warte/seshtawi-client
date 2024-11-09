1f5e43c94753cbb3ba97108e333bc0f819d31546
Marc <zwarte336@gmail.com>
2024-11-09 14:54:37 +0100

    update

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -41,7 +41,7 @@ export class HomepageComponent implements OnInit {
 
   populateArticles(){
 
-    this.http.get(`${environment.apiUrl_backend_dev}fetch_all`).pipe(map((response:any)=>{
+    this.http.get(`${environment.apiUrl_backend_prod}fetch_all`).pipe(map((response:any)=>{
       console.log(response);
 
       this.formatter(response);
