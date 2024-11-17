2e1e1d578d88c1db238653097af3fd61ed259a68
Marc <zwarte336@gmail.com>
2024-11-17 09:18:55 +0100

    responsive home

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,11 @@
 export const environment = {
 
       production: false, 
-      apiUrl_backend: 'http://localhost:3000/',
-      cdn_url:'http://localhost:4400/',
+      // apiUrl_backend: 'http://localhost:3000/',
+      // cdn_url:'http://localhost:4400/',
+      apiUrl_backend: 'https://zwarte-blog-backend-015f74c3dd24.herokuapp.com/',
+      apiUrl_backend_dev: 'http://localhost:4200/',
+      cdn_url:'https://multimedia-cdn-883c6c729d1a.herokuapp.com/',
 
 
 };
