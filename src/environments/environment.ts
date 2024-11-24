a7ea6e4a8dfbf6372dfa989807fe0c16bef08cc6
Marc <zwarte336@gmail.com>
2024-11-24 08:11:17 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
       apiUrl_backend: 'https://zwarte-blog-backend-015f74c3dd24.herokuapp.com/',
       apiUrl_backend_dev: 'http://localhost:4200/',
       cdn_url:'https://d2mlfq9ioop9wj.cloudfront.net/',
+      
 
 };
