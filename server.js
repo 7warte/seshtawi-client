ac1b593b027129fcae65868c895086d1fc9edea5
zwarte336 <zwarte336@gmail.com>
2025-02-15 13:14:52 +0100

    updates

--- server.js
+++ server.js
@@ -3,18 +3,30 @@ const app = express();
 require("dotenv").config();
 
 
+app.use(express.static('./dist/web_blog/browser'))
 
-app.use(express.static('./dist/web_blog'))
+app.get((req,res,next)=>{
 
-app.get('/*', (req,res,next)=>{
-    res.sendFile('index.html',{root:'dist/web_blog'})
+  
+     res.sendFile('index.html',{root:'dist/web_blog/browser'})
 })
 
-app.listen(process.env.PORT || 8080,function(){
 
 
-    
+app.get('/', (req,res,next)=>{
+
+    res.sendFile('index.html',{root:'dist/web_blog/browser/'})
+})
+
+
+app.get('/test', (req,res,next)=>{
+    console.log('test');
+    res.send({status:'ok'})
+})
+
+app.listen(parseInt(process.env.PORT) ,function(){
 
     console.log("⚡ Node app is running at localhost:" + (process.env.PORT));
+
 
 })
