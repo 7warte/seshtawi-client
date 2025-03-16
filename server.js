c6edadd99d9eb6e90dd86860038c36e77de887c0
Marc <marc@cleanupp.com>
2025-03-16 11:31:03 +0100

    update

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
 const express = require('express')
 const app = express();
 require("dotenv").config();
+const path = require('path')
 
+app.use(express.static(path.join(__dirname, 'dist/web_blog/browser')));
 
-app.use(express.static('./dist/web_blog/browser'))
-
-app.get((req,res,next)=>{
-
-  
-     res.sendFile('index.html',{root:'dist/web_blog/browser'})
-})
-
-
-
-app.get('/', (req,res,next)=>{
-
+app.get('*', (req,res,next)=>{
     res.sendFile('index.html',{root:'dist/web_blog/browser'})
 })
-
 
 app.get('/test', (req,res,next)=>{
     console.log('test');
     res.send({status:'ok'})
 })
-
 app.listen(parseInt(process.env.PORT) ,function(){
 
     console.log("⚡ Node app is running at localhost:" + (process.env.PORT));
