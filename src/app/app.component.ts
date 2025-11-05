abf9acf98a0aecf324c697c0777b027b913c4c1d
Marc <zwarte336@gmail.com>
2025-11-05 22:15:15 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {  Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -10,13 +11,25 @@ import {  Component, OnInit } from '@angular/core';
     standalone: false
 })
 export class AppComponent implements OnInit {
+
+
+  constructor(private router:Router){
+    console.log('testtt');
+  }
+
   title = 'web_blog';
   ngOnInit(): void {
-    
+  
 
+    
 
     console.log(window.screen.availWidth);
 
+
+    this.router.events.forEach((route:any)=>{
+      console.log(route);
+      
+    })
     
 
   }
