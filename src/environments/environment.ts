ff210bb3dbe52a9018487a7f084a337f32906f64
Marc <zwarte336@gmail.com>
2025-11-07 23:41:21 +0100

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,5 @@ export const environment = {
       apiUrl_backend: 'https://seshtawi-api-176cabdca928.herokuapp.com/',
       apiUrl_backend_dev: 'http://localhost:4200/',
       cdn_url:'https://d2mlfq9ioop9wj.cloudfront.net/',
-      
 
 };
