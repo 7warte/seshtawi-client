c783110b252c4d4347c02ceb3c30830a71fcc8b0
Marc <zwarte336@gmail.com>
2025-11-07 23:39:21 +0100

    endpoint update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
       production: true, 
-      apiUrl_backend: 'https://zwarte-blog-backend-015f74c3dd24.herokuapp.com/',
+      apiUrl_backend: 'https://seshtawi-api-176cabdca928.herokuapp.com/',
       apiUrl_backend_dev: 'http://localhost:4200/',
       cdn_url:'https://d2mlfq9ioop9wj.cloudfront.net/',
       
